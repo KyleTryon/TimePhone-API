afdbfa65e0cde3edd2408471b80714a83e0b6e62
KyleTryon <kyle.tryon@outlook.com>
2023-03-20 20:59:24 -0400

    feat!: prompts must now be submitted

--- src/shared/ai.ts
+++ src/shared/ai.ts
@@ -12,8 +12,7 @@ export class AI {
     this._api = new OpenAIApi(config);
   }
 
-  async startCall(character: string) {
-    const prompt = `You must pretend to be ${character}, you know their life history and will speak in their style. Begin the conversation as you would answer a phone in your new persona.`;
+  async startCall(prompt: string) {
     const response = await this._api.createChatCompletion({
       model: 'gpt-3.5-turbo',
       messages: [
