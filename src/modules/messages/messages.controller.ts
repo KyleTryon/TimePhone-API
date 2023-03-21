6215fc1b8c0de1da23235d90403d43bc51835813
KyleTryon <kyle.tryon@outlook.com>
2023-03-20 22:16:32 -0400

    chore: comment out unused operations

--- src/modules/messages/messages.controller.ts
+++ src/modules/messages/messages.controller.ts
@@ -11,7 +11,7 @@ import {
 } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
-import { UpdateMessageDto } from './dto/update-message.dto';
+// import { UpdateMessageDto } from './dto/update-message.dto';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiConsumes } from '@nestjs/swagger';
 
@@ -40,13 +40,13 @@ export class MessagesController {
     return this.messagesService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMessageDto: UpdateMessageDto) {
-    return this.messagesService.update(+id, updateMessageDto);
-  }
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateMessageDto: UpdateMessageDto) {
+  //   return this.messagesService.update(+id, updateMessageDto);
+  // }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.messagesService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.messagesService.remove(+id);
+  // }
 }
