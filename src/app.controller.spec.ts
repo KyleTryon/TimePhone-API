3dd089f467a804dc58bf62d6de0894e2e75efa4e
KyleTryon <kyle.tryon@outlook.com>
2023-03-20 22:22:36 -0400

    chore: update default message with docs link

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return API docs', () => {
+      expect(appController.getHello()).toContain(
+        '/api',
+      );
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return '<p>View the API documentation at <a href="/api">/api</a>.<br>View the GitHub repo at <a href="https://github.com/KyleTryon/TimePhone-API">https://github.com/KyleTryon/TimePhone-API</a></p>';
   }
 }
